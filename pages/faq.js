4c80aa09dccbe2ea29d2e6f3dc12bfc19af7524a
Wilson Santos <contato@wilsonsantos.net>
2022-03-02 15:12:06 -0300

    Add FAQ screen

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,6 @@
-import Link from '../src/components/Link';
-import PageTitle from '../src/components/PageTitle';
+import FAQScreen from '../src/screens/FAQScreen';
 
+export default FAQScreen;
 
 export async function getStaticProps() {
   const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
@@ -14,31 +14,4 @@ export async function getStaticProps() {
       faq,
     },
   };
-}
-
-export default function Faq({faq}) {
-
-
-  return (
-    <div>
-      <PageTitle>
-        FAQ - Alura cases campanha
-      </PageTitle>
-      <h1>Alura Cases - FAQ</h1>
-      <Link href="/">
-        Voltar para Home
-      </Link>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 }
